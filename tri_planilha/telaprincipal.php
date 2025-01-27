b126eed534de5faae1f55d5b9e82b15b181d265a
pedrovsz <161390737+pedrovsz@users.noreply.github.com>
2025-01-27 12:57:21 -0300

    Add files via upload

--- tri_planilha/telaprincipal.php
+++ tri_planilha/telaprincipal.php
@@ -4,8 +4,19 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Sistema Planilhas</title>
+    <link rel="stylesheet" href="style.css">
 </head>
 <body>
-    <label for=""></label>ola mundo</br><input>
+    <form action="excel.php" method="post">
+    <label for="material" name="material" class="material">Material</br><input></br></label>
+    <label for="descricao" name="descricao" class="descricao">Descrição</br><input></br></label>
+    <label for="quantidade" name="quantidade" class="quantidade">Quantidade</br><input></br></label>
+    </br><input type="submit" class="conf" value="Adicionar">
+    </form>
+
+    <?php
+    
+    ?>
+    
 </body>
 </html>
